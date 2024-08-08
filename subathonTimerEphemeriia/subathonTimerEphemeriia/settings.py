cb2f6ac6c1e02fbb5e41050bb4bb204177d782c7
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-08-09 01:40:20 +0200

    test debug

--- subathonTimerEphemeriia/subathonTimerEphemeriia/settings.py
+++ subathonTimerEphemeriia/subathonTimerEphemeriia/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # deepcode ignore HardcodedNonCryptoSecret: <please specify a reason of ignoring this>
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = [config("BACKEND_HOST", default="127.0.0.1")]
 
