5ad612d9708f7eef1e2f7e3ac484501fa901a6de
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-08-09 01:51:23 +0200

    test debug

--- subathonTimerEphemeriia/subathonTimerEphemeriia/settings.py
+++ subathonTimerEphemeriia/subathonTimerEphemeriia/settings.py
@@ -98,12 +98,13 @@ DATABASES = {
         conn_max_age=600,
         conn_health_checks=True
     ),
-} if not DEBUG else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+} 
+# if not DEBUG else {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
