27b5864173f526b9d84aa5ca9bb07041b5fdb4af
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-09-12 21:32:41 +0200

    feat: Add endpoint to add time to timer

--- subathonTimerEphemeriia/subathonTimer/urls.py
+++ subathonTimerEphemeriia/subathonTimer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import TimerViewSet, index, start_timer
+from .views import TimerViewSet, index, start_timer, add_time
 
 from rest_framework.routers import DefaultRouter
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('',index, name='index'),
     path('start_timer',start_timer, name='start_timer'),
+    path('add_time',add_time, name='add_time'),
 ]
--- subathonTimerEphemeriia/subathonTimer/views.py
+++ subathonTimerEphemeriia/subathonTimer/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpRequest
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated , IsAdminUser, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -13,18 +14,47 @@ from .utils import write_log
 def index(request):
     timer = Timer.objects.last()
     if timer is None:
-        time = 'no timer'
+        time = "no timer"
     else:
         time = timer.display_time()
         started = timer.timer_active
-    return render(request, 'index.html', {'time': time, 'started': started})
+    return render(request, "index.html", {"time": time, "started": started})
+
+
+def add_time(request):
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect("/admin_django/login/?next=/add_time/")
+
+        user = request.user
+
+        req = HttpRequest()
+        req.user = user
+        req.method = "POST"
+        req.data = {
+            "time": request.POST["time"],
+            "username": user.username,
+        }
+
+        tvs = TimerViewSet()
+
+        res = tvs.add_time(req)
+        
+        return render(request, "addTime.html", {"message": res.data["message"], "status": res.data["status"]})
+
+    elif request.method == "GET":
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect("/admin_django/login/?next=/add_time/")
+        return render(request, "addTime.html", {})
+
 
 def start_timer(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         timer = Timer.objects.last()
         timer.timer_active = True
         timer.save()
-        return redirect('index')
+        return redirect("index")
+
 
 class TimerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
@@ -33,53 +63,51 @@ class TimerViewSet(viewsets.ModelViewSet):
 
     seen_ids = set()
 
-    @action(detail=False, methods=['get'], permission_classes=[AllowAny])
+    @action(detail=False, methods=["get"], permission_classes=[AllowAny])
     def get_timer_info(self, request, pk=None):
         timer = Timer.objects.last()
         time = timer.display_time()
-        return Response({'time': time})
-    
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
-    def sub(self, request, pk=None):
+        return Response({"time": time})
 
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
+    def sub(self, request, pk=None):
         timer = Timer.objects.last()
 
-        tier = request.data['tier']
-        username = request.data['username']
-        id = request.data['id']
+        tier = request.data["tier"]
+        username = request.data["username"]
+        id = request.data["id"]
 
         if id in self.seen_ids:
-            return Response({'message': 'Already seen','status' : 400})
-        
+            return Response({"message": "Already seen", "status": 400})
+
         self.seen_ids.add(id)
 
         try:
             tier = int(tier)
 
-            if not timer.new_sub(tier) :
+            if not timer.new_sub(tier):
                 raise Exception
-            
+
         except Exception:
-            return Response({'message': 'Invalid tier','status' : 400})
+            return Response({"message": "Invalid tier", "status": 400})
 
         timer.send_ticket()
 
         write_log(f"New sub: {username} - {tier}")
 
-        return Response({'message': 'Sub added','status' : 200})
-    
+        return Response({"message": "Sub added", "status": 200})
 
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
     def bits(self, request, pk=None):
         timer = Timer.objects.last()
 
-        bits = request.data['bits']
-        username = request.data['username']
-        id = request.data['id']
+        bits = request.data["bits"]
+        username = request.data["username"]
+        id = request.data["id"]
 
         if id in self.seen_ids:
-            return Response({'message': 'Already seen','status' : 400})
-        
+            return Response({"message": "Already seen", "status": 400})
+
         self.seen_ids.add(id)
 
         try:
@@ -87,25 +115,24 @@ class TimerViewSet(viewsets.ModelViewSet):
             timer.new_bits(bits)
 
         except Exception:
-            return Response({'message': 'Invalid bits','status' : 400})
+            return Response({"message": "Invalid bits", "status": 400})
 
         timer.send_ticket()
 
         write_log(f"New bits: {username} - {bits}")
 
-        return Response({'message': 'Bits added','status' : 200})
-    
+        return Response({"message": "Bits added", "status": 200})
 
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
     def donation(self, request, pk=None):
         timer = Timer.objects.last()
-        donation = request.data['amount']
-        name = request.data['name']
-        id = request.data['id']
+        donation = request.data["amount"]
+        name = request.data["name"]
+        id = request.data["id"]
 
         if id in self.seen_ids:
-            return Response({'message': 'Already seen','status' : 400})
-        
+            return Response({"message": "Already seen", "status": 400})
+
         self.seen_ids.add(id)
 
         try:
@@ -113,29 +140,30 @@ class TimerViewSet(viewsets.ModelViewSet):
             timer.new_donation(donation)
 
         except Exception as e:
-            return Response({'message': 'Invalid donation','status' : 400})
+            return Response({"message": "Invalid donation", "status": 400})
 
         timer.send_ticket()
 
         write_log(f"New donation: {name} - {donation}")
 
-        return Response({'message': 'Donation added','status' : 200})
-    
+        return Response({"message": "Donation added", "status": 200})
 
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
     def add_time(self, request, pk=None):
         timer = Timer.objects.last()
-        time = request.data['time']
-        username = request.data['username'] if 'username' in request.data else 'anonymous'
+        time = request.data["time"]
+        username = (
+            request.data["username"] if "username" in request.data else "anonymous"
+        )
 
         try:
             timer.add_time(float(time))
 
         except Exception as e:
-            return Response({'message': 'Invalid time','status' : 400})
+            return Response({"message": "Invalid time", "status": 400})
 
         timer.send_ticket()
 
         write_log(f"{username} added time: {time} seconds")
 
-        return Response({'message': 'Time added','status' : 200})
+        return Response({"message": "Time added", "status": 200})
