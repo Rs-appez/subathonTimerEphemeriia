1cf530da7da80e84a08607558b412806925654a4
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-09-29 23:47:54 +0200

    fix url

--- subathonTimerEphemeriia/subathonTimer/urls.py
+++ subathonTimerEphemeriia/subathonTimer/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('',index, name='index'),
     path('start_timer',start_timer, name='start_timer'),
-    path('add_time',add_time, name='add_time'),
-    path('add_time_success',add_time_success, name='add_time_success'),
+    path('add_time/',add_time, name='add_time'),
+    path('add_time_success/',add_time_success, name='add_time_success'),
     path('tip_progress', tip_progress, name='tip_progress'),
     path('pause_timer', pause_timer, name='pause_timer'),
 ]
