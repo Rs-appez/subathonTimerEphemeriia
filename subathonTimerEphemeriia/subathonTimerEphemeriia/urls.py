31edb91ba8b931437d90cbf156eeb2bf0023196b
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-10-03 18:15:26 +0200

    set up new app

--- subathonTimerEphemeriia/subathonTimerEphemeriia/urls.py
+++ subathonTimerEphemeriia/subathonTimerEphemeriia/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('oauth/',include('oauth.urls')),
     path('',include('subathonTimer.urls')),
+    path('bingo/',include('bingo.urls')),
 ]
