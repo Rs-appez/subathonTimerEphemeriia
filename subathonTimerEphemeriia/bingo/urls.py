ff10d8e5cba5e606d50e921bbe59441995a8766c
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-10-04 02:16:13 +0200

    add endPoint to ctivate bingo

--- subathonTimerEphemeriia/bingo/urls.py
+++ subathonTimerEphemeriia/bingo/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
-from .views import index
+from .views import index, BingoViewSet
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'bingo', BingoViewSet, basename='bingo')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('', index, name='index'),
 ]
--- subathonTimerEphemeriia/bingo/views.py
+++ subathonTimerEphemeriia/bingo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from .models import Bingo, BingoItem, BingoItemUser, User
+from .serializers import BingoSerializer
 
 from math import sqrt
 import bleach
@@ -29,3 +34,16 @@ def index(request):
         return render(request, 'bingo/bingo.html', {'bingo': bingo, 'bingo_items': bingo_items, 'bingo_lenght': bingo_lenght})
     
     return  render(request, 'bingo/error.html', {'message': 'User not found'})
+
+
+class BingoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = Bingo.objects.all()
+    serializer_class = BingoSerializer
+
+    @action(detail=True, methods=['post'],permission_classes=[IsAdminUser])
+    def activate(self, request, pk=None):
+        bingo = self.get_object()
+        bingo.is_active = True
+        bingo.save()
+        return Response({'status': 'Bingo activated'})
