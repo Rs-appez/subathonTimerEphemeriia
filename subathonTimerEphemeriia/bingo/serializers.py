9ac1e49b32e2f0b77ca451f891f0bc08bc08aa31
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-10-07 02:40:23 +0200

    Refactor serializers to include BingoItemUser model

--- subathonTimerEphemeriia/bingo/serializers.py
+++ subathonTimerEphemeriia/bingo/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from .models import Bingo
+from .models import Bingo, BingoItemUser
 
 class BingoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bingo
+        fields = '__all__'
+
+class BingoItemUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BingoItemUser
         fields = '__all__'
