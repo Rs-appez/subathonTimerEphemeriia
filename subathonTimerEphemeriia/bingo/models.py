c7030506abeac4fd72111f7d2b4e3ef976f66e2d
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-10-07 20:10:17 +0200

    bingo no decription enable

--- subathonTimerEphemeriia/bingo/models.py
+++ subathonTimerEphemeriia/bingo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Bingo(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
