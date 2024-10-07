9a695a6092012d60dc173149afde41f448367a11
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-10-07 02:54:37 +0200

    Refactor BingoItemUserSerializer to include the BingoItem model and the 'is_checked' field

--- subathonTimerEphemeriia/bingo/serializers.py
+++ subathonTimerEphemeriia/bingo/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 
-from .models import Bingo, BingoItemUser
+from .models import Bingo, BingoItemUser, BingoItem
 
 class BingoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bingo
         fields = '__all__'
 
+class BingoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BingoItem
+        fields = ['name']
+
 class BingoItemUserSerializer(serializers.ModelSerializer):
+
+    bingo_item = BingoItemSerializer()
     class Meta:
         model = BingoItemUser
-        fields = '__all__'
+        fields = ['bingo_item', 'is_checked']
+
