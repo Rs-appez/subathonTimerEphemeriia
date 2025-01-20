2bc69b13cf1694af1253bedcfdfb29ce929d339d
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-20 10:18:08 +0100

    upgrade css

--- subathonTimerEphemeriia/static/bingo/js/bingo.js
+++ subathonTimerEphemeriia/static/bingo/js/bingo.js
@@ -1,4 +1,4 @@
-const data = document.currentScript.dataset;
-const bingo_length = JSON.parse(data.bingo_lenght);
-
-document.getElementById('bingoCard').style.gridTemplateColumns = `repeat(${bingo_length}, 1fr)`;
+//const data = document.currentScript.dataset;
+//const bingo_length = JSON.parse(data.bingo_lenght);
+//
+//document.getElementById('bingoCard').style.gridTemplateColumns = `repeat(${bingo_length}, 1fr)`;
