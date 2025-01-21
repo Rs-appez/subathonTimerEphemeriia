3c8243b81bd378354f515e4352096e3e7c2b1126
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-21 06:08:46 +0100

    add more span time to claim item

--- subathonTimerEphemeriia/bingo/models.py
+++ subathonTimerEphemeriia/bingo/models.py
@@ -4,6 +4,7 @@ import math
 
 import bleach
 
+
 # Create your models here.
 class Bingo(models.Model):
     name = models.CharField(max_length=100)
@@ -15,7 +16,6 @@ class Bingo(models.Model):
     def __str__(self):
         return self.name
 
-
     def reset_all_items(self):
         bingo_items = BingoItem.objects.filter(bingo=self)
         users = User.objects.all()
@@ -23,7 +23,8 @@ class Bingo(models.Model):
             bingo_item.desactivate_item()
         for user in users:
             user.reset_all_items()
-    
+
+
 class BingoItem(models.Model):
     name = models.CharField(max_length=100)
     bingo = models.ForeignKey(Bingo, on_delete=models.CASCADE)
@@ -31,28 +32,29 @@ class BingoItem(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def save(self):
         self.name = bleach.clean(self.name)
         return super().save()
-    
+
     def activate_item(self):
         self.is_active = True
         self.save()
         self.__auto_desactivate()
         return self.is_active
-    
+
     def desactivate_item(self):
         self.is_active = False
         self.save()
         return self.is_active
-    
+
     def __auto_desactivate(self):
-        min = 5
-        timer = threading.Timer(min*60, self.desactivate_item)
+        min = 6
+        timer = threading.Timer(min * 60, self.desactivate_item)
         timer.name = f"Auto desactivate item {self.id}"
-        timer.start()        
-    
+        timer.start()
+
+
 class User(models.Model):
     name = models.CharField(max_length=100)
     id_twitch = models.CharField(max_length=100)
@@ -61,14 +63,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     @staticmethod
     def create_with_bingoIteam(name, id_twitch, bingo):
         user = User.objects.create(name=name, id_twitch=id_twitch)
         user.get_new_items(bingo)
-        
+
         return user
-    
+
     def get_new_items(self, bingo):
         bingo_default_items = BingoItem.objects.filter(bingo=bingo).order_by("?")
         BingoItemUser.objects.filter(user=self).delete()
@@ -79,41 +81,41 @@ class User(models.Model):
     def reset_all_items(self):
         bingo = Bingo.objects.filter(is_active=True).last()
         self.get_new_items(bingo)
-        
+
         self.has_won = False
         self.save()
 
     def win(self):
         self.has_won = True
         self.save()
-    
+
+
 class BingoItemUser(models.Model):
     bingo_item = models.ForeignKey(BingoItem, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.bingo_item}'
-    
+        return f"{self.bingo_item}"
+
     def check_item(self):
 
         # self.is_checked = not self.is_checked
         # self.save()
         # return self.is_checked
-    
-        if not self.is_checked :
+
+        if not self.is_checked:
             self.is_checked = self.bingo_item.is_active
             self.save()
         return self.is_checked
 
-    
     def check_bingo(self):
         bingo_items = BingoItemUser.objects.filter(user=self.user)
 
         size = int(math.sqrt(len(bingo_items)))
 
         for i in range(size):
-            line = bingo_items[i*size:(i+1)*size]
+            line = bingo_items[i * size : (i + 1) * size]
             if all([item.is_checked for item in line]):
                 return True
 
@@ -121,11 +123,11 @@ class BingoItemUser(models.Model):
             if all([item.is_checked for item in column]):
                 return True
 
-        diagonal1 = bingo_items[::size+1]
+        diagonal1 = bingo_items[:: size + 1]
         if all([item.is_checked for item in diagonal1]):
             return True
-        
-        diagonal2 = bingo_items[size-1::size-1][:size]
+
+        diagonal2 = bingo_items[size - 1 :: size - 1][:size]
         if all([item.is_checked for item in diagonal2]):
             return True
 
