155d6def3cce703f752af81c0680143fbe1a6801
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-21 23:20:06 +0100

    add smooth animtion

--- subathonTimerEphemeriia/static/bingo/js/bingo.js
+++ subathonTimerEphemeriia/static/bingo/js/bingo.js
@@ -1,6 +1,21 @@
 const data = document.currentScript.dataset;
 
 let show_bingo = data.show_bingo === "True";
-let display = show_bingo ? `block` : `none `;
+let bingoElement = document.getElementsByClassName("bingo-show")[0];
 
-document.getElementsByClassName("bingo-show")[0].style.display = display;
+if (show_bingo) {
+    bingoElement.classList.add("visible");
+} else {
+    bingoElement.classList.remove("visible");
+}
+
+function toggle_bingo() {
+    show_bingo = !show_bingo;
+    if (show_bingo) {
+        bingoElement.classList.add("visible");
+    } else {
+        bingoElement.classList.remove("visible");
+    }
+}
+
+document.getElementById("showBingo").addEventListener("click", toggle_bingo);
