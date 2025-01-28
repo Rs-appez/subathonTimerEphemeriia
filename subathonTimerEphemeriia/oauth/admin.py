c88f0f7eac8197cf1a1ba4fd291297e870723d7c
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-28 09:04:03 +0100

    add chatbot in admin

--- subathonTimerEphemeriia/oauth/admin.py
+++ subathonTimerEphemeriia/oauth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import TwitchAuth, StreamlabsAuth
+from .models import TwitchAuth, StreamlabsAuth, ChatbotAuth
 
 admin.site.register(TwitchAuth)
 admin.site.register(StreamlabsAuth)
+admin.site.register(ChatbotAuth)
