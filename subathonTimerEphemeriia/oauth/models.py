8c42f3c5441de4add6dca6ca218910904ceb8dcd
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-01-28 09:03:22 +0100

    add model/serializer for chatbot

--- subathonTimerEphemeriia/oauth/models.py
+++ subathonTimerEphemeriia/oauth/models.py
@@ -4,6 +4,7 @@ from decouple import config
 
 # Create your models here.
 
+
 class TwitchAuth(models.Model):
     access_token = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255)
@@ -15,29 +16,33 @@ class TwitchAuth(models.Model):
 
     def __str__(self):
         return self.access_token
-    
+
     def refresh(self):
-        res = requests.post('https://id.twitch.tv/oauth2/token', params={
-            'client_id': config("TWITCH_APP_ID"),
-            'client_secret': config("TWITCH_APP_SECRET"),
-            'grant_type': 'refresh_token',
-            'refresh_token': self.refresh_token
-        })
+        res = requests.post(
+            "https://id.twitch.tv/oauth2/token",
+            params={
+                "client_id": config("TWITCH_APP_ID"),
+                "client_secret": config("TWITCH_APP_SECRET"),
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token,
+            },
+        )
 
         if res.status_code == 200:
             data = res.json()
-            self.access_token = data['access_token']
-            self.refresh_token = data['refresh_token']
-            self.expires_in = data['expires_in']
-            self.token_type = data['token_type']
-            self.scope = data['scope']
+            self.access_token = data["access_token"]
+            self.refresh_token = data["refresh_token"]
+            self.expires_in = data["expires_in"]
+            self.token_type = data["token_type"]
+            self.scope = data["scope"]
             self.save()
         else:
-            
+
             return False
 
         return True
-    
+
+
 class StreamlabsAuth(models.Model):
     access_token = models.CharField(max_length=2083)
     refresh_token = models.CharField(max_length=2083)
@@ -49,3 +54,41 @@ class StreamlabsAuth(models.Model):
 
     def __str__(self):
         return self.access_token
+
+
+class ChatbotAuth(models.Model):
+    access_token = models.CharField(max_length=2083)
+    refresh_token = models.CharField(max_length=2083)
+    expires_in = models.IntegerField()
+    token_type = models.CharField(max_length=255)
+    scope = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.access_token)
+
+    def refresh(self):
+        res = requests.post(
+            "https://id.twitch.tv/oauth2/token",
+            params={
+                "client_id": config("TWITCH_APP_ID"),
+                "client_secret": config("TWITCH_APP_SECRET"),
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token,
+            },
+        )
+
+        if res.status_code == 200:
+            data = res.json()
+            self.access_token = data["access_token"]
+            self.refresh_token = data["refresh_token"]
+            self.expires_in = data["expires_in"]
+            self.token_type = data["token_type"]
+            self.scope = data["scope"]
+            self.save()
+        else:
+
+            return False
+
+        return True
--- subathonTimerEphemeriia/oauth/serializers.py
+++ subathonTimerEphemeriia/oauth/serializers.py
@@ -1,12 +1,20 @@
-from .models import TwitchAuth, StreamlabsAuth
+from .models import TwitchAuth, StreamlabsAuth, ChatbotAuth
 from rest_framework import serializers
+
 
 class TwitchAuthSerializer(serializers.ModelSerializer):
     class Meta:
         model = TwitchAuth
-        fields = '__all__'
+        fields = "__all__"
+
 
 class StreamlabsAuthSerializer(serializers.ModelSerializer):
     class Meta:
         model = StreamlabsAuth
-        fields = '__all__'
+        fields = "__all__"
+
+
+class ChatbotAuthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatbotAuth
+        fields = "__all__"
