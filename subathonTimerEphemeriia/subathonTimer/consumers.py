9d3ab987e3ed4d0717006be676639cd504b170f4
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-27 09:35:14 +0100

    update to manage multiple group

--- subathonTimerEphemeriia/subathonTimer/consumers.py
+++ subathonTimerEphemeriia/subathonTimer/consumers.py
@@ -1,29 +1,29 @@
-from django.conf import settings
 from asgiref.sync import async_to_sync
 from channels.consumer import SyncConsumer
 
+
 class TicksSyncConsumer(SyncConsumer):
+    def __get_group_name(self):
+        return self.scope["url_route"]["kwargs"]["group_name"]
 
     def websocket_connect(self, event):
-        self.send({
-            'type': 'websocket.accept'
-        })
+        self.send({"type": "websocket.accept"})
 
         # Join ticks group
         async_to_sync(self.channel_layer.group_add)(
-            settings.TICKS_GROUP_NAME,
-            self.channel_name
+            self.__get_group_name(), self.channel_name
         )
 
     def websocket_disconnect(self, event):
         # Leave ticks group
         async_to_sync(self.channel_layer.group_discard)(
-            settings.TICKS_GROUP_NAME,
-            self.channel_name
+            self.__get_group_name(), self.channel_name
         )
 
     def new_ticks(self, event):
-        self.send({
-            'type': 'websocket.send',
-            'text': event['content'],
-        })
+        self.send(
+            {
+                "type": "websocket.send",
+                "text": event["content"],
+            }
+        )
--- subathonTimerEphemeriia/subathonTimer/routing.py
+++ subathonTimerEphemeriia/subathonTimer/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/ticks/$', consumers.TicksSyncConsumer.as_asgi()),
+    re_path(r"ws/(?P<group_name>\w+)/$", consumers.TicksSyncConsumer.as_asgi()),
 ]
