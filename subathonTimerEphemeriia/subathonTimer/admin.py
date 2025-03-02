372319b6777ace3d9606e1d1025da03c1eb326ae
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-02 06:44:27 +0100

    format

--- subathonTimerEphemeriia/subathonTimer/admin.py
+++ subathonTimerEphemeriia/subathonTimer/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 from .models import Timer, BonusTime, TipGoal, SubGoal
 
-class GoalAdmin(admin.ModelAdmin):
 
-    list_display = ( "goal_name",'goal_amount')
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ("goal_name", "goal_amount")
+
 
 admin.site.register(Timer)
 admin.site.register(BonusTime)
-admin.site.register(TipGoal,GoalAdmin)
-admin.site.register(SubGoal,GoalAdmin)
+admin.site.register(TipGoal, GoalAdmin)
+admin.site.register(SubGoal, GoalAdmin)
