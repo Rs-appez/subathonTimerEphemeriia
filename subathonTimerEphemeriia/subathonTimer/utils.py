9e164e6ea1c336b62998d830a9fbe952223a4f52
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-05 20:05:34 +0100

    move get_logs in utils

--- subathonTimerEphemeriia/subathonTimer/utils.py
+++ subathonTimerEphemeriia/subathonTimer/utils.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 
+
 def write_log(msg):
-    path = "/logs/log.txt" if not  settings.DEBUG else "log.txt" 
+    path = "/logs/log.txt" if not settings.DEBUG else "log.txt"
     with open(path, "a") as f:
         f.write("\n")
         f.write(msg)
         f.write("\n")
         f.write("-" * 100)
+
+
+def get_logs():
+    path = "/logs/log.txt" if not settings.DEBUG else "log.txt"
+    with open(path, "r") as f:
+        lines = f.readlines()
+    return lines[::-1]
