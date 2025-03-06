5cdc56321d23d448fcf31dcb7db76780f26b0d99
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-06 03:47:36 +0100

    add get_tippers

--- subathonTimerEphemeriia/subathonTimer/utils.py
+++ subathonTimerEphemeriia/subathonTimer/utils.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+import re
+
 
 def write_log(msg):
     path = "/logs/log.txt" if not settings.DEBUG else "log.txt"
@@ -15,3 +17,25 @@ def get_logs():
     with open(path, "r") as f:
         lines = f.readlines()
     return lines[::-1]
+
+
+def get_tippers():
+    logs = get_logs()
+    tippers = []
+
+    for log in logs:
+        start_sub = re.search(r"Subathon started", log)
+        if start_sub:
+            break
+        match = re.search(r"New donation: (\w+) - (\d+\.\d+)", log)
+        if match:
+            for tipper in tippers:
+                if tipper["name"] == match.group(1):
+                    tipper["total"] = float(tipper["total"]) + float(match.group(2))
+                    break
+
+            else:
+                tippers.append({"name": match.group(1), "total": float(match.group(2))})
+
+    tippers = sorted(tippers, key=lambda x: x["total"], reverse=True)
+    return tippers
