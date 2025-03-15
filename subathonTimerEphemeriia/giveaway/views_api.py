cb82be890933095850f83250e966eb79b12b5377
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-15 04:59:11 +0100

    add api view

---
+++ subathonTimerEphemeriia/giveaway/views_api.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+
+from .models import Calendar, Cell, Reward
+from .serializers import CalendarSerializer, CellSerializer, RewardSerializer
+
+class CalendarViewSet(viewsets.ModelViewSet):
+    authentication_classes = [IsAdminUser]
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+
+class CellViewSet(viewsets.ModelViewSet):
+    authentication_classes = [IsAdminUser]
+    queryset = Cell.objects.all()
+    serializer_class = CellSerializer
+
+class RewardViewSet(viewsets.ModelViewSet):
+    authentication_classes = [IsAdminUser]
+    queryset = Reward.objects.all()
+    serializer_class = RewardSerializer
