3f4428af40b9129d46c3308ead195988bd5fdde3
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-15 04:23:40 +0100

    add model in admin

--- subathonTimerEphemeriia/giveaway/admin.py
+++ subathonTimerEphemeriia/giveaway/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Calendar, Cell, Reward
 
-# Register your models here.
+admin.site.register(Calendar)
+admin.site.register(Cell)
+admin.site.register(Reward)
