612f03c3ccdc1066c58a1e825f02fb63d0624062
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-20 17:03:53 +0100

    send calendar to html

--- subathonTimerEphemeriia/giveaway/views.py
+++ subathonTimerEphemeriia/giveaway/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import Calendar
+from .serializers import CalendarSerializer
+
+
 def index(request):
-    return render(request, "giveaway/index.html")
+    calendar = Calendar.objects.filter(is_active=True).last()
+    calendar_json = CalendarSerializer(calendar).data
+
+    return render(request, "giveaway/index.html", {"calendar": calendar_json})
