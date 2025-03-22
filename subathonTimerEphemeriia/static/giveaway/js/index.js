8d0b8aabcb3b16ae3b4a6498345c88b929fdd7c8
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-03-22 22:50:47 +0100

    add rdm animation on click

--- subathonTimerEphemeriia/static/giveaway/js/index.js
+++ subathonTimerEphemeriia/static/giveaway/js/index.js
@@ -2,27 +2,51 @@ const rawCellData = document.getElementById("calendar-cells-data").textContent;
 const cellsData = JSON.parse(rawCellData);
 
 function handleClick(event) {
-  event.preventDefault();
-  // Your click handling logic here
-  const cellNumber = event.target.id.split("_")[1];
+    event.preventDefault();
+    // Your click handling logic here
+    const cellNumber = event.target.id.split("_")[1];
 
-  console.log(cellNumber);
+    let cell_img = document.getElementById(`cell_${cellNumber}`);
+
+    cell_img.addEventListener("animationend", function() {
+        event.target.remove();
+        cell_img.remove();
+    });
+    addRandomEffect(cell_img);
+
+    console.log(cellNumber);
+}
+
+function addRandomEffect(element) {
+    const effects = ["go-up", "fade-out"];
+    const randomEffect = effects[Math.floor(Math.random() * effects.length)];
+
+    // add specific effect
+    switch (randomEffect) {
+        case "go-up":
+            element.style.setProperty("z-index", "1");
+            break;
+        default:
+            break;
+    }
+
+    element.classList.add(randomEffect);
 }
 
 function scaleImage() {
-  cellsData.forEach(function (cell) {
-    var img = document.getElementById(`cell_${cell.number}`);
-    var imgWidth = img.width;
-    var imgHeight = img.height;
+    cellsData.forEach(function(cell) {
+        var img = document.getElementById(`cell_${cell.number}`);
+        var imgWidth = img.width;
+        var imgHeight = img.height;
 
-    var originalCoords = cell.coordonates.split(",").map(Number);
-    var coords = originalCoords.map(function (coord, index) {
-      return index % 2 === 0 ? coord * imgWidth : coord * imgHeight;
+        var originalCoords = cell.coordonates.split(",").map(Number);
+        var coords = originalCoords.map(function(coord, index) {
+            return index % 2 === 0 ? coord * imgWidth : coord * imgHeight;
+        });
+
+        var area = document.getElementById(`cell_${cell.number}_area`);
+        area.setAttribute("coords", coords.join(","));
     });
-
-    var area = document.getElementById(`cell_${cell.number}_area`);
-    area.setAttribute("coords", coords.join(","));
-  });
 }
 
 window.addEventListener("load", scaleImage);
