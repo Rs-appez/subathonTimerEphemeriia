ffa1da9589927a942d89bd855a0fb4a2ab594fba
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-15 01:04:31 +0200

    rework model wip

--- subathonTimerEphemeriia/giveaway/models.py
+++ subathonTimerEphemeriia/giveaway/models.py
@@ -7,7 +7,7 @@ class Calendar(models.Model):
 
     mask_url = models.URLField()
     background_url = models.URLField()
-    cells = models.ManyToManyField("Cell", related_name="cells")
+    cells = models.ManyToManyField("CalendarCell", related_name="cells")
 
     is_active = models.BooleanField(default=False)
 
@@ -15,23 +15,31 @@ class Calendar(models.Model):
         return str(self.title)
 
 
-class Cell(models.Model):
-    number = models.IntegerField()
-    image_url = models.URLField()
+class CalendarCell(models.Model):
+    cell = models.ForeignKey(
+        "Cell", on_delete=models.CASCADE, related_name="calendar_cells"
+    )
     reward = models.ForeignKey(
         "Reward", on_delete=models.CASCADE, null=True, blank=True
     )
 
     is_opened = models.BooleanField(default=False)
 
+    def open(self):
+        self.is_opened = True
+        self.save()
+
+
+class Cell(models.Model):
+    number = models.IntegerField()
+    size = models.IntegerField()
+
+    image_url = models.URLField()
+
     coordonates = models.CharField(max_length=500)
 
     def __str__(self):
         return str(self.number)
-
-    def open(self):
-        self.is_opened = True
-        self.save()
 
 
 class Reward(models.Model):
