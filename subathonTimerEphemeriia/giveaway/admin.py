6c6f79ba725dbb3110d7f38c073cba75e0740886
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-17 05:48:59 +0200

    add new model to admin

--- subathonTimerEphemeriia/giveaway/admin.py
+++ subathonTimerEphemeriia/giveaway/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Calendar, Cell, Reward
+from .models import Calendar, Cell, Reward, CalendarCell
 
 admin.site.register(Calendar)
 admin.site.register(Cell)
 admin.site.register(Reward)
+admin.site.register(CalendarCell)
