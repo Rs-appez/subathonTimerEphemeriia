73ad422a2bf10a62182c03ed4140b502880a0cce
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-19 12:23:53 +0200

    add endpoint to create calendar

--- subathonTimerEphemeriia/giveaway/views_api.py
+++ subathonTimerEphemeriia/giveaway/views_api.py
@@ -3,14 +3,43 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
-from .models import Calendar, Cell, Reward
-from .serializers import CalendarSerializer, CellSerializer, RewardSerializer
+from .models import Calendar, Cell, Reward, BaseCalendar
+from .serializers import (
+    CalendarSerializer,
+    CellSerializer,
+    RewardSerializer,
+    BaseCalendarSerializer,
+)
+
+import bleach
 
 
 class CalendarViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
+
+    @action(
+        detail=False,
+        methods=["post"],
+        permission_classes=[IsAdminUser],
+        url_path="create",
+    )
+    def create_calendar(self, request):
+        title = bleach.clean(request.data.get("title"))
+        base_calendar_id = request.data.get("base_calendar_id")
+        background_url = bleach.clean(request.data.get("background_url"))
+
+        baseCalendar = BaseCalendar.objects.get(id=base_calendar_id)
+        if not baseCalendar:
+            return Response({"status": "BaseCalendar not found"}, status=404)
+
+        calendar = Calendar.objects.create(
+            title=title, background_url=background_url, base_calendar=baseCalendar
+        )
+        calendar.generate_cells()
+
+        return Response({"status": "calendar created", "calendar_id": calendar.id})
 
 
 class CellViewSet(viewsets.ModelViewSet):
