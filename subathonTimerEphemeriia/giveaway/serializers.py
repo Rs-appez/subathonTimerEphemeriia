117ceff321940ef8a1bdcf099fb849149498ee79
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-19 07:40:02 +0200

    update serializers with new model

--- subathonTimerEphemeriia/giveaway/serializers.py
+++ subathonTimerEphemeriia/giveaway/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Calendar, Cell, Reward
+from .models import Calendar, Cell, Reward, BaseCalendar, CalendarCell
 
 
 class CellSerializer(serializers.ModelSerializer):
@@ -15,8 +15,25 @@ class RewardSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class CalendarCellSerializer(serializers.ModelSerializer):
+    cell = CellSerializer()
+    reward = RewardSerializer()
+
+    class Meta:
+        model = CalendarCell
+        fields = "__all__"
+
+
+class BaseCalendarSerializer(serializers.ModelSerializer):
+    cells = CalendarCellSerializer(many=True)
+
+    class Meta:
+        model = BaseCalendar
+        fields = "__all__"
+
+
 class CalendarSerializer(serializers.ModelSerializer):
-    cells = CellSerializer(many=True)
+    base_calendar = BaseCalendarSerializer()
 
     class Meta:
         model = Calendar
