37199bc9f3860db52ce760f4a308fdd3ab8be467
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-19 12:23:14 +0200

    add baseCalendar

--- subathonTimerEphemeriia/giveaway/admin.py
+++ subathonTimerEphemeriia/giveaway/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Calendar, Cell, Reward, CalendarCell
+from .models import Calendar, Cell, Reward, CalendarCell, BaseCalendar
 
 admin.site.register(Calendar)
 admin.site.register(Cell)
 admin.site.register(Reward)
 admin.site.register(CalendarCell)
+admin.site.register(BaseCalendar)
