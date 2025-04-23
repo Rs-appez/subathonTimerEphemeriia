42cb35a2f6dce1c615b3111f41ef00fc19769c79
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-23 21:41:12 +0200

    add btn to close cells

--- subathonTimerEphemeriia/static/giveaway/js/update_calendar.js
+++ subathonTimerEphemeriia/static/giveaway/js/update_calendar.js
@@ -1,11 +1,12 @@
 import { getCookie } from "/static/general/js/utils.js";
 
 const shuffleButton = document.getElementById("shuffle-button");
+const closeAllButton = document.getElementById("close-button");
 const calendarId = document.getElementById("calendar-id-data").textContent;
 
-function shuffle() {
-    var csrftoken = getCookie("csrftoken");
+const csrftoken = getCookie("csrftoken");
 
+function shuffle() {
     fetch(`/giveaway/api/calendar/${calendarId}/shuffle_reward/`, {
         method: "POST",
         headers: {
@@ -23,4 +24,23 @@ function shuffle() {
         });
 }
 
+function closeAll() {
+    fetch(`/giveaway/api/calendar/${calendarId}/close_all/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrftoken,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+            window.location.reload();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
+
 shuffleButton.addEventListener("click", shuffle);
+closeAllButton.addEventListener("click", closeAll);
