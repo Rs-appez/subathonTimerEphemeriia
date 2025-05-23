ce3b29d33aa377ec5c31282f69c47e5c81b39fdd
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-23 19:42:53 +0200

    unallow overwrite

--- subathonTimerEphemeriia/subathonTimerEphemeriia/storage_backends.py
+++ subathonTimerEphemeriia/subathonTimerEphemeriia/storage_backends.py
@@ -3,9 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class RewardStorage(S3Boto3Storage):
     location = "rewards"
-    file_overwrite = True
+    file_overwrite = False
 
 
 class BackgroundStorage(S3Boto3Storage):
     location = "backgrounds"
-    file_overwrite = True
+    file_overwrite = False
