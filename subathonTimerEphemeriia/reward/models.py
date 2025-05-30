4f804ab026f31425860f3e88fa7aea86c62df9be
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-05-30 21:54:05 +0200

    add reward model

--- subathonTimerEphemeriia/reward/models.py
+++ subathonTimerEphemeriia/reward/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Reward(models.Model):
+    reward_id = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
