2835c30ae85f1e81300d32cb20a261a0fe80f01c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-02 19:28:22 +0200

    add CarAnn to admin

--- subathonTimerEphemeriia/subathonTimer/admin.py
+++ subathonTimerEphemeriia/subathonTimer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Timer, BonusTime, TipGoal, SubGoal
+from .models import Timer, BonusTime, TipGoal, SubGoal, CarouselAnnouncement
 
 
 class GoalAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ admin.site.register(Timer)
 admin.site.register(BonusTime)
 admin.site.register(TipGoal, GoalAdmin)
 admin.site.register(SubGoal, GoalAdmin)
+admin.site.register(CarouselAnnouncement)
