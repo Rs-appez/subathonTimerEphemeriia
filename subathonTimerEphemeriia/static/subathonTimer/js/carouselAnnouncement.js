c9a88749ff518e7115fa9fa945da0eaad8f39e88
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-03 00:26:05 +0200

    add v1 carouselAnnoucement

---
+++ subathonTimerEphemeriia/static/subathonTimer/js/carouselAnnouncement.js
@@ -0,0 +1,33 @@
+const announcements = document.getElementById("announcements");
+const announcementData = JSON.parse(announcements.innerHTML);
+
+let i = 0;
+
+function rotateAnnouncements() {
+    if (i >= announcementData.length) {
+        i = 0;
+    }
+    const element = announcementData[i];
+    let announcement = document.getElementById("ann_" + element.id);
+    if (announcement) {
+        showAnnouncement(announcement);
+        setTimeout(() => {
+            hideAnnouncement(announcement);
+            setTimeout(() => {
+                rotateAnnouncements();
+            }, 3000);
+        }, element.duration * 1000);
+    }
+    i++;
+}
+
+function showAnnouncement(announcement) {
+    announcement.classList.remove("hide");
+    announcement.classList.add("show");
+}
+function hideAnnouncement(announcement) {
+    announcement.classList.remove("show");
+    announcement.classList.add("hide");
+}
+
+rotateAnnouncements();
