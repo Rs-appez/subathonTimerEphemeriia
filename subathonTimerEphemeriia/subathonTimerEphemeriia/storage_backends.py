29f023048fd83d599b22c09c9b3e8838a4b1a990
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-02 18:55:26 +0200

    add misc storage

--- subathonTimerEphemeriia/subathonTimerEphemeriia/storage_backends.py
+++ subathonTimerEphemeriia/subathonTimerEphemeriia/storage_backends.py
@@ -1,6 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
+class MiscStorage(S3Boto3Storage):
+    location = "misc"
+    file_overwrite = False
+
+
 class RewardStorage(S3Boto3Storage):
     location = "rewards"
     file_overwrite = False
