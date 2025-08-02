7b1d35b16b49c92b6d8fe7d67d59a32510387cb8
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-02 20:10:18 +0200

    add CarAnn seralizer

--- subathonTimerEphemeriia/subathonTimer/serializers.py
+++ subathonTimerEphemeriia/subathonTimer/serializers.py
@@ -1,12 +1,20 @@
-from .models import Timer, TipGoal
+from .models import Timer, TipGoal, CarouselAnnouncement
 from rest_framework import serializers
+
 
 class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
-        fields = '__all__'
+        fields = "__all__"
+
 
 class TipGoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipGoal
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CarouselAnnouncementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarouselAnnouncement
+        fields = "__all__"
