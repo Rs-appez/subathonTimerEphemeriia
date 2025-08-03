44eee88188f84b6587f4ec9c8bf29596b9c9d130
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-03 18:48:05 +0200

    oups x)

--- subathonTimerEphemeriia/static/subathonTimer/js/update_timer.js
+++ subathonTimerEphemeriia/static/subathonTimer/js/update_timer.js
@@ -250,7 +250,7 @@ function resizeDisplayGoals() {
 }
 
 // Websocket
-var ws_url = "ws://" + window.location.host + "/ws/ticks/";
+var ws_url = "wss://" + window.location.host + "/ws/ticks/";
 
 function connect() {
     var ws = new WebSocket(ws_url);
