39aea050d514dad10a236682093f6aa64c07cef6
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-20 12:11:11 +0200

    add active field to wheel

--- subathonTimerEphemeriia/wheel/models.py
+++ subathonTimerEphemeriia/wheel/models.py
@@ -6,14 +6,24 @@ from utils.utils import rename_file_to_upload
 
 class Whell(models.Model):
     name = models.CharField(max_length=100)
+    active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
+    def activate(self):
+        self.active = True
+        self.save()
+        for wheel in Whell.objects.exclude(id=self.id):
+            wheel.deactivate()
+
+    def deactivate(self):
+        self.active = False
+        self.save()
+
 
 class Entry(models.Model):
-    whell = models.ForeignKey(
-        Whell, on_delete=models.CASCADE, related_name="entries")
+    whell = models.ForeignKey(Whell, on_delete=models.CASCADE, related_name="entries")
     text = models.CharField(max_length=255)
     number = models.IntegerField(default=1)
     image = models.ImageField(
