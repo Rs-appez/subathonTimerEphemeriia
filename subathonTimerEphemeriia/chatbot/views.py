5aa85b8280123f6331d29c96c7141d74353e702c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-08-29 15:28:41 +0200

    use serializers

--- subathonTimerEphemeriia/chatbot/views.py
+++ subathonTimerEphemeriia/chatbot/views.py
@@ -4,11 +4,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 
 from .models import Bot
+from .serializers import BotSerializer
 
 
 class BotViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
     queryset = Bot.objects.all()
+    serializer_class = BotSerializer
 
     @action(detail=True, methods=["post"], permission_classes=[IsAdminUser])
     def speak(self, request, pk=None):
