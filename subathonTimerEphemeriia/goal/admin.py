83d684910800fd1ad59bc41ed93b646fd71ff26d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-17 21:12:12 +0200

    add goal to admin

--- subathonTimerEphemeriia/goal/admin.py
+++ subathonTimerEphemeriia/goal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Campaign, Goal
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    class GoalInline(admin.TabularInline):
+        model = Goal
+        extra = 3
+
+    inlines = [GoalInline]
