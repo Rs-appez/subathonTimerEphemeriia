242619908874aa324384ca7df42bec9bea6049ea
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-17 21:06:57 +0200

    add models

--- subathonTimerEphemeriia/goal/models.py
+++ subathonTimerEphemeriia/goal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Campaign(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Goal(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name='goals')
+    title = models.CharField(max_length=200)
+    goal = models.FloatField()
+
+    def __str__(self):
+        return f"{self.title} - {self.goal}"
