c1ce2d1a5260c576b8b842ec23e1984b7780f91d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-18 01:09:13 +0200

    add progress view

--- subathonTimerEphemeriia/goal/urls.py
+++ subathonTimerEphemeriia/goal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views_api import CampaignViewSet, GoalViewSet
+from . import views as v
 
 router = DefaultRouter()
 router.register(r"campaigns", CampaignViewSet, basename="campaign")
@@ -9,4 +10,5 @@ router.register(r"goals", GoalViewSet, basename="goal")
 
 urlpatterns = [
     path("api/", include(router.urls)),
+    path("progress/<int:campaign_id>/", v.campaign_progress, name="campaign_progress"),
 ]
