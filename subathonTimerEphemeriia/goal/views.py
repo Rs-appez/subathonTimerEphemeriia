b3277feee1e475882651a699cb35e72bf1ac6551
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-18 01:09:13 +0200

    add progress view

--- subathonTimerEphemeriia/goal/views.py
+++ subathonTimerEphemeriia/goal/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Campaign
+from .serializers import CampaignSerializer
+
+
+@permission_required("subathonTimer.view_campaign")
+def campaign_progress(request, campaign_id):
+    campaign = get_object_or_404(Campaign, id=campaign_id, is_active=True)
+    campaign_data = CampaignSerializer(campaign).data
+
+    return render(
+        request,
+        "campaignProgress.html",
+        {
+            "campaign": campaign_data,
+        },
+    )
