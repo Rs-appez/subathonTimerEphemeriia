88d7941254ba568df93f01d57c3298833db7c1cb
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-26 16:53:37 +0100

    add check on id

--- subathonTimerEphemeriia/goal/views_api.py
+++ subathonTimerEphemeriia/goal/views_api.py
@@ -17,19 +17,27 @@ class CampaignViewSet(ModelViewSet):
     serializer_class = CampaignSerializer
     permission_classes = [DjangoModelPermissions]
 
+    seen_ids = set()
+
     @action(detail=True, methods=["get"])
     def update_campaign(self, request, pk=None):
         campaign = self.get_object()
         send_campaign_update(campaign)
         return Response({"message": "Campaign update sent.", "status": 200})
 
-    @action(detail=False, methods=["post"])
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
     def update_progress(self, request, pk=None):
         campaigns = Campaign.objects.filter(is_active=True)
         if not campaigns.exists():
             return Response({"message": "No active campaign.", "status": 400})
 
         amount = request.data.get("amount")
+        id = request.data.get("id")
+
+        if id in self.seen_ids:
+            return Response({"message": "Already seen", "status": 400})
+        self.seen_ids.add(id)
+
         try:
             amount = float(amount)
         except (TypeError, ValueError):
