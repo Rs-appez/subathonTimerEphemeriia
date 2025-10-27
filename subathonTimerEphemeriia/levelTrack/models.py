0669955a15171f597d757fdd39aa4d35c059e32c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-27 18:10:27 +0100

    add method to update level

--- subathonTimerEphemeriia/levelTrack/models.py
+++ subathonTimerEphemeriia/levelTrack/models.py
@@ -5,3 +5,6 @@ class LevelTracker(models.Model):
     target_level = models.IntegerField()
     started_at = models.DateTimeField(auto_now_add=True)
 
+    def update_level(self, new_level: int):
+        self.current_level = new_level
+        self.save(update_fields=["current_level"])
