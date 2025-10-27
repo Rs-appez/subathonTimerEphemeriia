4d84a2dd2fe8536c1a61edb85f99e13aa994754b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-27 18:10:38 +0100

    add admin view

--- subathonTimerEphemeriia/levelTrack/urls.py
+++ subathonTimerEphemeriia/levelTrack/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views as v
 
-urlpatterns = [path("<int:id>/", v.index, name="bar_tracker")]
+urlpatterns = [
+    path("<int:id>/", v.index, name="bar_tracker"),
+    path("admin/<int:id>/", v.admin_view, name="bar_tracker_admin"),
+]
--- subathonTimerEphemeriia/levelTrack/views.py
+++ subathonTimerEphemeriia/levelTrack/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.generics import get_object_or_404
+from django.http import  HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import LevelTracker
 from .serializers import LevelTrackerSerializer
@@ -10,3 +11,19 @@ def index(request, id: int):
     lt = get_object_or_404(LevelTracker, pk=id)
     lt_data = LevelTrackerSerializer(lt).data
     return render(request, "index.html", {"tracker": lt_data})
+
+
+@permission_required("levelTrack.change_leveltracker")
+def admin_view(request, id: int):
+    lt = get_object_or_404(LevelTracker, pk=id)
+
+    if request.method == "POST":
+        current_level = request.POST.get("new_level")
+        try:
+            current_level = int(current_level)
+        except (TypeError, ValueError):
+            return HttpResponse("Invalid current level", status=400)
+        lt.update_level(current_level)
+        return redirect("bar_tracker_admin", id=lt.id)
+
+    return render(request, "admin_view.html", {"tracker": lt})
