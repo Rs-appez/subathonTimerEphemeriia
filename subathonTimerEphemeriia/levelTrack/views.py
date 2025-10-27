33ae7064391e7a1063f73d7186c9eeb485a3cfef
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-27 17:12:28 +0100

    add perm

--- subathonTimerEphemeriia/levelTrack/views.py
+++ subathonTimerEphemeriia/levelTrack/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.generics import get_object_or_404
+from django.contrib.auth.decorators import permission_required
 from .models import LevelTracker
 from .serializers import LevelTrackerSerializer
 
 
+@permission_required("levelTrack.view_leveltracker")
 def index(request, id: int):
     lt = get_object_or_404(LevelTracker, pk=id)
     lt_data = LevelTrackerSerializer(lt).data
