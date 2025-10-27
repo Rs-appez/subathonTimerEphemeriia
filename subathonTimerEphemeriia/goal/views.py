70c2f0d18da158f95f9b1a21610ba85afecc4e31
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-27 16:56:35 +0100

    fix perm

--- subathonTimerEphemeriia/goal/views.py
+++ subathonTimerEphemeriia/goal/views.py
@@ -5,7 +5,7 @@ from .models import Campaign
 from .serializers import CampaignSerializer
 
 
-@permission_required("subathonTimer.view_campaign")
+@permission_required("goal.view_campaign")
 def campaign_progress(request, campaign_id):
     campaign = get_object_or_404(Campaign, id=campaign_id, is_active=True)
     campaign_data = CampaignSerializer(campaign).data
