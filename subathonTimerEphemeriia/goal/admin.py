1c7a0d4b073c813ec035ac9e62645012b76972bd
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 00:06:46 +0100

    feat(goal): add CampaignType and GoalIcon models
    - Introduced CampaignType and GoalIcon models for better campaign and goal
      categorization and icon management.
    - Updated Campaign and Goal models to use new foreign keys for type and icon.
    - Added is_target_hidden field to Campaign for hiding target amounts.
    - Refactored serializers to include and flatten related type and icon fields.
    - Registered new models in Django admin for management.
    - Added and updated migrations for new models and fields.

--- subathonTimerEphemeriia/goal/admin.py
+++ subathonTimerEphemeriia/goal/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Campaign, Goal
+from .models import Campaign, Goal, CampaignType, GoalIcon
+
+admin.site.register(CampaignType)
+admin.site.register(GoalIcon)
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
