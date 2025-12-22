280ddb1bbb75ae89197eda667aaa0f860e368c8c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-12-22 07:28:24 +0100

    remove old websocket code

--- subathonTimerEphemeriia/static/subathonTimer/js/totalSub.js
+++ subathonTimerEphemeriia/static/subathonTimer/js/totalSub.js
@@ -1,5 +1,5 @@
 const data = document.currentScript.dataset;
-var total_subs = data.total_subs;
+let total_subs = data.total_subs;
 
 function updateSub() {
     const subs = document.getElementById("subs");
@@ -9,29 +9,10 @@ function updateSub() {
     }
 }
 
-// Websocket
-var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
-var ws_url = ws_scheme + "://" + window.location.host + "/ws/ticks/";
+const evtSource = new EventSource("/timer/events/");
+evtSource.onmessage = function(event) {
+    const data_ws = JSON.parse(event.data);
+    total_subs = data_ws.total_subscriptions;
 
-function connect() {
-    var ws = new WebSocket(ws_url);
-
-    ws.onmessage = function(event) {
-        var data_ws = JSON.parse(event.data);
-        total_subs = data_ws.total_subscriptions;
-
-        updateSub();
-    };
-
-    ws.onclose = function(event) {
-        console.log("Connection closed");
-        connect();
-    };
-
-    ws.onerror = function(event) {
-        console.log("Error");
-        ws.close();
-    };
-}
-
-connect();
+    updateSub();
+};
--- subathonTimerEphemeriia/static/subathonTimer/js/update_tip_progress.js
+++ subathonTimerEphemeriia/static/subathonTimer/js/update_tip_progress.js
@@ -1,49 +1,28 @@
 const data = document.currentScript.dataset;
-var total_tips = data.total_tips;
-var last_goal = data.last_goal;
+let total_tips = data.total_tips;
+let last_goal = data.last_goal;
 
 function updateTipProgress() {
-    const tipProgress = document.getElementById('progress-tip');
-    const tipLeft = document.getElementById('tip-left');
-    const tipRight = document.getElementById('tip-right');
+    const tipProgress = document.getElementById("progress-tip");
+    const tipLeft = document.getElementById("tip-left");
+    const tipRight = document.getElementById("tip-right");
 
     if (tipProgress && tipLeft && tipRight) {
         next_goal = last_goal;
 
-        tipProgress.value = (total_tips / next_goal * 100); 
+        tipProgress.value = (total_tips / next_goal) * 100;
         tipLeft.innerHTML = total_tips;
         tipRight.innerHTML = next_goal;
     }
-
 }
 
-// Websocket
-var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
-var ws_url = ws_scheme + "://" + window.location.host + "/ws/ticks/";
+const evtSource = new EventSource("/timer/events/");
+evtSource.onmessage = function(event) {
+    const data_ws = JSON.parse(event.data);
+    total_tips = data_ws.total_tips;
+    total_subscriptions = data_ws.total_subscriptions;
 
-function connect() {
-    var ws = new WebSocket(ws_url);
+    updateTipProgress();
+};
 
-    ws.onmessage = function (event) {
-        var data_ws = JSON.parse(event.data);
-        total_tips = data_ws.total_tips;
-        total_subscriptions = data_ws.total_subscriptions;
-
-        updateTipProgress();
-
-    }
-
-    ws.onclose = function (event) {
-        console.log('Connection closed');
-        connect();
-
-    }
-
-    ws.onerror = function (event) {
-        console.log('Error');
-        ws.close();
-    }
-}
-
-connect();
 updateTipProgress();
