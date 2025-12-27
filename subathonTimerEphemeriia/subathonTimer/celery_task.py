0c73c258038c3b71f158683bc642a7afad45d87b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-12-27 12:52:01 +0100

    use one client

--- subathonTimerEphemeriia/subathonTimer/celery_task.py
+++ subathonTimerEphemeriia/subathonTimer/celery_task.py
@@ -8,19 +8,15 @@ from django.conf import settings
 
 
 redis_pool = ConnectionPool.from_url(settings.CELERY_BROKER_URL)
-
-def redis_client() -> Redis:
-    return Redis(connection_pool=redis_pool)
+redis_client = Redis(connection_pool=redis_pool)
 
 @shared_task
 def send_update(data):
-    client = redis_client()
     payload = json.dumps(data) if isinstance(data, dict) else str(data)
-    client.publish(Channels.UPDATES.value, payload)
+    redis_client.publish(Channels.UPDATES.value, payload)
 
 
 @shared_task
 def send_start_event(data):
-    client = redis_client()
     payload = json.dumps(data) if isinstance(data, dict) else str(data)
-    client.publish(Channels.START_EVENT.value, payload)
+    redis_client.publish(Channels.START_EVENT.value, payload)
