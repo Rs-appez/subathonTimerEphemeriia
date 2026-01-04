33cb636dee6b6bad8fe20c4babfc62aa97d6a898
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-01-05 00:27:29 +0100

    feat(giveaway): add loading state to calendar actions
    Add loading state to the calendar UI when "Shuffle" or "Close all" buttons
    are pressed. Buttons are disabled and a loading class is applied during
    the request, improving user feedback for these actions.

--- subathonTimerEphemeriia/static/giveaway/js/update_calendar.js
+++ subathonTimerEphemeriia/static/giveaway/js/update_calendar.js
@@ -2,6 +2,39 @@ import { getCookie } from "/static/general/js/utils.js";
 
 const csrftoken = getCookie("csrftoken");
 
+const calendarContainer = document.getElementById("cells-container");
+const shuffleButton = document.getElementById("shuffle-button");
+const closeAllButton = document.getElementById("close-all-button");
+
 document.body.addEventListener("htmx:configRequest", (event) => {
     event.detail.headers["X-CSRFToken"] = csrftoken;
 });
+
+function loadingCalendar() {
+    calendarContainer.classList.add("loading");
+    shuffleButton.disabled = true;
+    closeAllButton.disabled = true;
+}
+
+function loadedCalendar() {
+    calendarContainer.classList.remove("loading");
+    shuffleButton.disabled = false;
+    closeAllButton.disabled = false;
+}
+
+function onRequest(event) {
+    if (["shuffle-button", "close-all-button"].includes(event.target.id)) {
+        loadingCalendar();
+    }
+}
+
+function onResponse(event) {
+    if (
+        ["shuffle-button", "close-all-button"].includes(event.requestConfig.elt.id)
+    ) {
+        loadedCalendar();
+    }
+}
+
+document.body.addEventListener("htmx:beforeRequest", onRequest);
+document.body.addEventListener("htmx:afterSwap", onResponse);
